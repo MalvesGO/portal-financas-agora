1bd14789adb654f95005fc4d0ef6819a6af885cd
marcelo.alves <marcelo.alves@pt.clara.net>
2024-03-30 22:30:08 +0000

    searchbar

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,11 +1,12 @@
-import { InputSearch } from "@ama-pt/agora-design-system";
+import { InputSearch, InputSearchBar } from "@ama-pt/agora-design-system";
 import React from "react";
 
 export default function Search() {
   return (
-    <div className="px-16 py-16">
-      <InputSearch
+    <div className="px-16 py-16">      
+      <InputSearchBar
         placeholder="Indique o que pretende... (Ex: Entregar IRS)"
+        voiceActionAltText="Descreva por palavras o que pretende procurar..."
         className="mx-16"
       />
     </div>
